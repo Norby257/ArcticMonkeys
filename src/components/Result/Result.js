576e10144739754fb771c825ee43cb2368254ff6
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-06-11 19:49:37 -0500

    adding result component

---
+++ src/components/Result/Result.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react';
+
+//  this is basically a table that contains the songs from whatever album that they click on 
+//  cuz that would be cool right?
+//  or, even better, pull results from Spotify API and display on table -round two 
+
+//  setting to empty array so we can push info there and map over it to rend to screen 
+
+class Result extends Component {
+    state = {
+        results: [],
+        albumName: {
+
+        },
+        trackName: []
+    }
+}
+
+//  
+
+export default Result;
